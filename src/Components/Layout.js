921f93d8709bc93675dbe77e12f75d6b97795b21
Sumit <dy4366190@gmail.com>
2021-09-14 12:41:32 +0530

    push

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import './../Styles/layout.css'
 
+
 const Layout = ({children}) =>{
     return(
         <div className="container">
