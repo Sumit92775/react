3678541fafe626be35aee0dcea2604257a896256
Sumit <dy4366190@gmail.com>
2021-09-14 13:19:27 +0530

    first commit

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import './../Styles/layout.css'
-import './page'
+import './page';
 
 
 const Layout = ({children}) =>{
